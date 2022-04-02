33e81f357739f7726476022093e7353d1869908f
Marcos Sabatini <marcossabatini7@gmail.com>
2022-04-02 17:28:43 -0300

    fix routes

--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -6,9 +6,9 @@ import (
 )
 
 var RegisterBookRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
 }
