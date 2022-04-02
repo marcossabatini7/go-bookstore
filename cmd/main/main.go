e40982f10d283766be01422491c48220a30de610
Marcos Sabatini <marcossabatini7@gmail.com>
2022-04-02 14:53:29 -0300

    register book routes and start the server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/marcossabatini7/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	routes.RegisterBookRoutes(r)
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
