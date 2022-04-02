b087d9c9d99249aa933f70bbe43a03e2ffe0788e
Marcos Sabatini <marcossabatini7@gmail.com>
2022-04-02 15:08:47 -0300

    Add book model

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,44 @@
 package models
+
+import (
+	"github.com/marcossabatini7/go-bookstore/pkg/config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name" gorm:"name"`
+	Author      string `json:"author" gorm:"author"`
+	Publication string `json:"publication" gorm:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) GetAll() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var book Book
+	db := db.Where("ID=?", Id).Find(&book)
+	return &book, db
+}
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
+}
