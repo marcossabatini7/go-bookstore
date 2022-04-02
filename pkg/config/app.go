76386e1f38f72a88d08b66763d84dee90298528b
Marcos Sabatini <marcossabatini7@gmail.com>
2022-04-02 14:24:32 -0300

    connect to the database

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "host=localhost user=admin password=podman dbname=g_bookstore port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+
+	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
